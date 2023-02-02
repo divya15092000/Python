249761498a731b450a0e46f2eb2f2a7b7e686ce7
Divya Saini <124121090+divya15092000@users.noreply.github.com>
2023-02-02 10:13:01 +0530

    McCulloch-Pitts  neural network using AND function

---
+++ andf.py
@@ -0,0 +1,12 @@
+a = int(input("enter the 1st number"))
+b = int(input("enter the 2st number"))
+w1 = 1 
+w2 = 1
+threshold = 2
+ain = a*w1 + b*w2
+if(ain>=threshold):
+    print(1)
+else:
+    print(0)
+
+
